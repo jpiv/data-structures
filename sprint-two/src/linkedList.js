3ca2016a9c8eba1cfd118b57bbdfd98aa3f954f7
jpiv <jjpizzo4@gmail.com>
2015-03-26 16:05:57 -0700

    Add linked list and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+    if(list.tail === null)
+    {
+      list.head = node;
+      list.tail = node;
+    }
+    else
+    {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head)
+    {
+      var result = list.head;
+      list.head = list.head.next;
+      return result.value;
+    }
+    return null;
   };
 
   list.contains = function(target){
+    var node = list.head;
+    while(node)
+    {
+      if(node.value === target)
+        return true;
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,14 +15,30 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target, subTree, accumulator){
+  accumulator = accumulator || false;
+  subTree = subTree || this;
 
+  for(var i = 0; i < subTree.children.length; i++)
+  {
+    if(subTree.children[i].value === target)
+      accumulator = true;
+
+    accumulator = subTree.contains(target, subTree.children[i], accumulator);
+    if(accumulator)
+      return true;
+  }
+
+  return accumulator;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//addChild: constant
+//contains: n
